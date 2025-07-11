aaeb2a835a56929fc6c29ca68870455ab9518fd7
Shayan Ahmadi <shayanahmadi2004@gmail.com>
2025-07-11 18:32:41 +0330

    user implementaion finished

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.repo.save(createUserDto);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const updateResult: UpdateResult = await this.repo.update(
+      id,
+      updateUserDto,
+    );
+    if (updateResult.affected === 0) throw new NotFoundException();
+    return this.repo.findBy({ id });
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) throw new NotFoundException();
+    this.repo.delete(id);
+    return user;
+  }
+}
