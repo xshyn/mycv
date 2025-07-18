c242bf2ab2629035590cba07f6af72a3e1c7c978
xshyn <shayanahmadi2004@gmail.com>
2025-07-18 09:23:21 +0330

    Update CurrentUserInterceptor to handle cases where session may be undefined, ensuring robust user retrieval.

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -11,7 +11,7 @@ export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
   async intercept(context: ExecutionContext, next: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
     if (userId) {
       const user = await this.usersService.findOne(userId);
       request.user = user;
