0d93274276b31f100a4a58c9d7dfe89017841239
Shayan Ahmadi <shayanahmadi2004@gmail.com>
2025-07-18 15:39:59 +0330

    Add test for user signup in AuthService to verify password salting and hashing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -25,4 +25,15 @@ describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
+  it('creates a new user with as salted and hashed password', async () => {
+    const user = await service.signup({
+      email: 'asdf@asdf.com',
+      password: 'asdf',
+    });
+
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
