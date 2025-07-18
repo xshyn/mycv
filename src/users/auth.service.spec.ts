9a6b4b97de9f66dd09716a5678206255bac292ad
Shayan Ahmadi <shayanahmadi2004@gmail.com>
2025-07-18 15:13:15 +0330

    Update test:watch script in package.json to limit Jest max workers to 1, and refactor AuthService tests to use CreateUserDto for user creation

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 it('can create an instance of auth service', async () => {
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
+    create: ({ email, password }: CreateUserDto) =>
       Promise.resolve({ id: 1, email, password }),
   };
 
