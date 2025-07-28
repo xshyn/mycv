f1ed3b3b2a250278cafd1d0dbba373c47c7dc451
Shayan Ahmadi <shayanahmadi2004@gmail.com>
2025-07-28 12:35:39 +0330

    feat(report): add up validation logic and prepared create report route in use

--- src/reports/dto/create-report.dto.ts
+++ src/reports/dto/create-report.dto.ts
@@ -1,1 +1,33 @@
-export class CreateReportDto {}
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  id: number;
+  @IsNumber()
+  price: number;
+  @IsString()
+  make: string;
+  @IsString()
+  model: string;
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import { AuthGuard } from 'src/users/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createReportDto: CreateReportDto) {
     return this.reportsService.create(createReportDto);
   }
